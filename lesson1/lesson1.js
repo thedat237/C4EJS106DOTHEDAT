9c50bf4260825819a72251041a8fadaeb15029f3
C4EJS106DOTHEDAT <dat9d3@gmail.com>
2021-01-23 19:46:34 +0700

    Hello

---
+++ lesson1/lesson1.js
@@ -0,0 +1,12 @@
+// let name=prompt("Họ và tên: ");
+// let age=prompt("Tuổi: ");
+// let years=prompt("Ngày tháng năm sinh: ");
+
+// console.log("Họ và tên:"+ name.toUpperCase());
+// console.log("Tuổi:"+ age);
+// console.log("Ngày tháng năm sinh:"+years);
+// // console.log(`Họ và tên:${name}. Tuoi:${age}`);
+
+let a=prompt("nhap so:");
+a=String(a);
+console.log(typeof(a));
