b6b76ff2b2d29cb85f3c76d7c8dbcd45e2d3f881
C4EJS106DOTHEDAT <dat9d3@gmail.com>
2021-03-22 10:06:32 +0700

    bai tap thu7/20/3/2021

--- lesson7FUNCTION&DOM/homeworklesson7/homework.js
+++ lesson7FUNCTION&DOM/homeworklesson7/homework.js
@@ -2,27 +2,27 @@
 // viết hàm tính tbc điểm của học sinh
 // alert ra màn hình học lực của học sinh đó
 
-// function diemtbc(...others){
-//     let tong=0;
-//     for(let i of others){
-//         tong+=i
-//     }
-//     let dtb=tong/4;
-//     console.log(dtb);
-//     if(dtb>=9){
-//         alert(`Điểm trung bình:${dtb} => điểm giỏi`)
-//     }
-//     else if(dtb>=7 && dtb<9){
-//         alert(`Điểm trung bình:${dtb} => điểm khá`)
-//     }
-//     else if(dtb>=5 && dtb<7){
-//         alert(`Điểm trung bình:${dtb} => điểm trung bình`)
-//     }
-//     else{
-//         alert(`Điểm trung bình:${dtb} => điểm yếu`)
-//     }
-// }
-// diemtbc(8,6,9,7)
+function diemtbc(...others){
+    let tong=0;
+    for(let i of others){
+        tong+=i
+    }
+    let dtb=tong/others.length;
+    console.log(dtb);
+    if(dtb>=9){
+        alert(`Điểm trung bình:${dtb} => điểm giỏi`)
+    }
+    else if(dtb>=7 && dtb<9){
+        alert(`Điểm trung bình:${dtb} => điểm khá`)
+    }
+    else if(dtb>=5 && dtb<7){
+        alert(`Điểm trung bình:${dtb} => điểm trung bình`)
+    }
+    else{
+        alert(`Điểm trung bình:${dtb} => điểm yếu`)
+    }
+}
+diemtbc(8,6,9,7)
 
 // 2.
 // "Viết hàm giải phương trình bậc 2 một ẩn: ax2 + bx + c = 0
